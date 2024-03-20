5d4384346a9ad8b2129493f7ea615f1c61e440f9
Saba Nizam <160767347+SabaNizam90@users.noreply.github.com>
2024-03-20 22:19:57 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//numeric DataTypes
+//string datatypes
+//boolean datatypes
+var box = 80;
+console.log(box);
+var myname = "Saba";
+console.log(myname);
+var isold = true;
+console.log(isold);
---
+++ main.ts
@@ -0,0 +1,12 @@
+//numeric DataTypes
+//string datatypes
+//boolean datatypes
+
+let box:number = 80;
+console.log(box)
+
+let myname:string = "Saba"
+console.log(myname)
+
+const isold:boolean =true
+console.log(isold)
